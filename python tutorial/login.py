ab1844613aa73bd171642c1592340ad6a8f25924
O_Ukandu <100975029+Ukandu20@users.noreply.github.com>
2023-04-21 04:10:45 -0400

    simple login system

--- python tutorial/login.py
+++ python tutorial/login.py
@@ -1,7 +1,20 @@
 #Simple Signup/Login System
+print("Create Account")
 username = input("Username: ")
 password = input("Password: ")
 
 print("Your account has been created Succesfully!")
+print("Login to your account")
 
-    
+username1 = input("Username: ")
+password1 = input("Password: ")
+
+#Check if password is correct
+
+
+if username == username1 and password == password1:
+    print("You have logged in Succesfully!")
+else:
+    print("Incorrect Username or Password")
+
+
